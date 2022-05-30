a0aad1866ea09b9fa935849b4cc4eb7169eef06f
eclipseOfTheEra <van9malibu@gmail.com>
2022-05-31 00:05:12 +0300

    pizza_v2_0.0.3

--- my-app/src/components/Categories.jsx
+++ my-app/src/components/Categories.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Categories() {
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острая', 'Закрытые'];
+
+	const onClickCategory = (index) => {
+		setActiveIndex(index);
+	};
+
 	return (
-		<div class='categories'>
+		<div className='categories'>
 			<ul>
-				<li class='active'>Все</li>
-				<li>Мясные</li>
-				<li>Вегетарианская</li>
-				<li>Гриль</li>
-				<li>Острые</li>
-				<li>Закрытые</li>
+				{categories.map((value, index) => (
+					<li
+						onClick={() => onClickCategory(index)}
+						className={activeIndex === index ? 'active' : ''}>
+						{value}
+					</li>
+				))}
 			</ul>
 		</div>
 	);
+}
+{
+	/* <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? 'active' : ''}>
+	Все
+</li>; */
 }
